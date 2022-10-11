6a87caf947d2ad5e111755a2c63aad2afd50b7d7
ErlendKH <s349571@oslomet.no>
2022-10-11 16:50:12 +0200

    a

---
+++ script.js
@@ -0,0 +1,78 @@
+let tl0;
+let tl1;
+let tl2;
+let tl3;
+let tl4;
+let tl5;
+let tl6;
+let tl7;
+let tl8;
+let tl9;
+
+let tr0;
+let tr1;
+let tr2;
+let tr3;
+let tr4;
+let tr5;
+let tr6;
+let tr7;
+let tr8;
+let tr9;
+
+let bl0;
+let bl1;
+let bl2;
+let bl3;
+let bl4;
+let bl5;
+let bl6;
+let bl7;
+let bl8;
+let bl9;
+
+let br0;
+let br1;
+let br2;
+let br3;
+let br4;
+let br5;
+let br6;
+let br7;
+let br8;
+let br9;
+
+$(document).ready(function(){
+
+    $("#tl0").click(function(){
+
+    })
+
+    $("#tl0").click(function(){
+
+    })
+
+});
+
+function resetTopLeft(){
+    
+}
+
+function resetTopRight(){
+
+}
+
+function resetBottomLeft(){
+
+}
+
+function resetBottomRight(){
+
+}
+
+function resetAll(){
+    resetTopLeft();
+    resetTopRight();
+    resetBottomLeft();
+    resetBottomRight();
+}
